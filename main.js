5fb6cd5b13a1219a6cb0ae04cb2a0cd0ab7a0356
jerryowusu <jerryowusu50@gmail.com>
2023-09-17 01:03:50 +0000

    Add the categories, courses and faqs section

---
+++ main.js
@@ -0,0 +1,21 @@
+const navbar = document.querySelector('nav');
+const faqs = document.querySelectorAll('.faq');
+
+window.addEventListener('scroll', () => {
+    navbar.classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+faqs.forEach((faq) => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        const icon = faq.querySelector('.faq-icon i') 
+
+            if (icon.className === 'uil uil-plus') {
+                icon.className = 'uil uil-minus'
+            } else {
+                icon.className = 'uil uil-plus'
+            }
+        }
+    )
+})
