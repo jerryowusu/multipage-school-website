8368b0925b11d396fae4b87f7403f579793cf063
jerryowusu <jerryowusu50@gmail.com>
2023-09-18 11:59:40 +0000

    Update tablet styling

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+//  variables/constants
+
 const navbar = document.querySelector('nav');
 const faqs = document.querySelectorAll('.faq');
+
+//  add scroll effect to menu bar
 
 window.addEventListener('scroll', () => {
     navbar.classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+//  change faqs icons
 
 faqs.forEach((faq) => {
     faq.addEventListener('click', () => {
@@ -19,4 +25,26 @@ faqs.forEach((faq) => {
         }
     )
 })
+
+// show and hide menu bar
+
+const menu = document.querySelector('.nav-menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+const openNav = () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+}
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+menuBtn.addEventListener('click', openNav)
+closeBtn.addEventListener('click', closeNav)
+
 
